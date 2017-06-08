9184eac5bbb0bbf1727f23bbd2161d71cbdd915a
huabingtao <1273621932@qq.com>
2017-06-08 19:51:52 +0800

    inside内页
    inside内页

--- src/vuex/index.js
+++ src/vuex/index.js
@@ -1,18 +1,7 @@
 // 定义state，并将state与store的各个属性关联起来
+
 import Vue from 'vue'
 import Vuex from 'vuex'
-<<<<<<< HEAD
-import menu from './modules/menu'
-Vue.use(Vuex)
-
-const store = new Vuex.Store({
-  modules: {
-    menu
-  },
-})
-
-export default store;
-=======
 
 import * as getters from './getters'
 import * as actions from './actions'
@@ -21,7 +10,9 @@ import * as mutations from './mutations'
 Vue.use(Vuex)
 
 const state = {
-    count: 0,
+    menu_off:false, //menu按钮
+    newList:true,//contentList判断是首页还是其他页面
+    newListId:0
 }
 
 const store = new Vuex.Store({
@@ -32,4 +23,3 @@ const store = new Vuex.Store({
 })
 
 export default store
->>>>>>> origin/master
